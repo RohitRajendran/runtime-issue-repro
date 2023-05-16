83ed4c576a98669b04219a8ea0ce5248fb6eda68
Rohit Rajendran <rohit.rajendran@outlook.com>
2023-05-15 22:24:05 -0400

    Make scheduled api route

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log("hi")
   res.status(200).json({ name: 'John Doe' })
 }
+export const config = {
+  type: 'experimental-scheduled',
+  schedule: '0 */6 * * *',
+};
